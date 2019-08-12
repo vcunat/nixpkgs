a046d7f42e565aae215060af36e6454e64e32f73
Luka Blaskovic <lblasc@tvbeat.com>
2019-08-12 13:22:06 +0000

    bootstrap-files: bump WIP

--- pkgs/stdenv/linux/bootstrap-files/x86_64.nix
+++ pkgs/stdenv/linux/bootstrap-files/x86_64.nix
@@ -1,9 +1,12 @@
-# Use busybox for i686-linux since it works on x86_64-linux as well.
-(import ./i686.nix) //
-
 {
+  busybox = import <nix/fetchurl.nix> {
+    url = https://lblasc-nix-dev.s3-eu-west-1.amazonaws.com/0zaxybl83qf0qc8bd3qb6z084jx94xv0-stdenv-bootstrap-tools/on-server/busybox;
+    sha256 = "1jyhi0irfgd8c8hwck3ym2qn8v29s3jm6a3cfilvilxjcblhzngx";
+    executable = true;
+  };
+
   bootstrapTools = import <nix/fetchurl.nix> {
-    url = http://tarballs.nixos.org/stdenv-linux/x86_64/4907fc9e8d0d82b28b3c56e3a478a2882f1d700f/bootstrap-tools.tar.xz;
-    sha256 = "abe3f0727dd771a60b7922892d308da1bc7b082afc13440880862f0c8823c09f";
+    url = https://lblasc-nix-dev.s3-eu-west-1.amazonaws.com/0zaxybl83qf0qc8bd3qb6z084jx94xv0-stdenv-bootstrap-tools/on-server/bootstrap-tools.tar.xz;
+    sha256 = "0pgl58xh0rbmcpvwwbd9z3jy1d957v0pvqxgj7w3m551d9d9d83p";
   };
 }
