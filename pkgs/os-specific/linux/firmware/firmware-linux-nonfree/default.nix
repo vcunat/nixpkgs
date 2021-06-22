ab357f64d2c94c8bf73084aa06d06b1dece019f4
Vladimír Čunát <v@cunat.cz>
2021-06-22 13:11:41 +0200

    firmware-linux-nonfree: revert amdgpu firmwares

--- pkgs/os-specific/linux/firmware/firmware-linux-nonfree/default.nix
+++ pkgs/os-specific/linux/firmware/firmware-linux-nonfree/default.nix
@@ -2,13 +2,26 @@
 
 stdenvNoCC.mkDerivation rec {
   pname = "firmware-linux-nonfree";
-  version = "2021-05-11";
+  version = "2021-05-11-amdgpu";
 
   src = fetchgit {
     url = "https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git";
     rev = "refs/tags/" + lib.replaceStrings ["-"] [""] version;
     sha256 = "015hajf3mq8dv2hw5wsyvi34zdqiwxp9p5dwdp8nrk4r9z5ysqxw";
   };
+
+  # Use older amdgpu firmware due to crashes #126771
+  postPatch = let
+    version_prev = "2021-03-15";
+    src_prev = fetchgit {
+      url = "https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git";
+      rev = "refs/tags/" + lib.replaceStrings ["-"] [""] version_prev;
+      sha256 = "sha256-BnYqveVFJk/tVYgYuggXgYGcUCZT9iPkCQIi48FOTWc=";
+    };
+  in ''
+    rm -r amdgpu/
+    cp -r '${src_prev}/amdgpu' ./amdgpu
+  '';
 
   installFlags = [ "DESTDIR=$(out)" ];
 
@@ -17,7 +30,7 @@ stdenvNoCC.mkDerivation rec {
 
   outputHashMode = "recursive";
   outputHashAlgo = "sha256";
-  outputHash = "034bwbl616vzl7lhcbvyz9dzmpzwi12vca3r5mszdxqh3z3s1g6a";
+  outputHash = "0q2kl61jwj81zd2bwh3m7adphf6fbjpfmb8wqc6i5xipn22a0h0j";
 
   meta = with lib; {
     description = "Binary firmware collection packaged by kernel.org";
