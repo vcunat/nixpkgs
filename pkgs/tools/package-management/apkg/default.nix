86ee0506d4499bb1e9831ff0ffb49d19aa3d7d5f
Vladimír Čunát <v@cunat.cz>
2021-07-02 19:30:32 +0200

    WIP apkg: 0.1.1 -> 0.1.1.dev6

--- pkgs/tools/package-management/apkg/default.nix
+++ pkgs/tools/package-management/apkg/default.nix
@@ -1,24 +1,38 @@
-{ lib, fetchFromGitLab, python3Packages }:
+{ lib, fetchFromGitLab, python3Packages
+, gitMinimal, rpm, dpkg, fakeroot
+}:
 
 python3Packages.buildPythonApplication rec {
   pname = "apkg";
-  version = "0.1.1";
+  version = "0.1.1.dev6"; # first master version supporting nix pkgstyle
 
   src = fetchFromGitLab {
     domain = "gitlab.nic.cz";
     owner = "packaging";
     repo = pname;
-    rev = "v${version}";
-    sha256 = "0fwylw1dpaq37wqzrggcvh8hrjcxhmfws5xljwffaa2abnd9y85d";
+    rev = "12ae2a2";
+    sha256 = "1cnmb9gwipl3g0ga582y8xiykk45q6d9frz8s7hsikarm56p4r7j";
   };
 
-  propagatedBuildInputs = with python3Packages; [ # see requirements.txt
-    # CORE REQUIREMENTS
-    cached-property click distro jinja2 packaging requests toml
-    # OPTIONAL REQUIREMENTS
-    build
-    # perhaps hard requirements?
-    htmllistparse blessings
+  propagatedBuildInputs = with python3Packages; [
+    # copy&pasted requirements.txt (almost exactly)
+    blessings        # terminal colors
+    build            # apkg distribution
+    cached-property  # for python <= 3.7; but pip complains even with 3.8
+    click            # nice CLI framework
+    distro           # current distro detection
+    htmllistparse    # upstream version detection
+    jinja2           # templating
+    packaging        # version parsing
+    requests         # HTTP for humans™
+    toml             # config files
+
+    # further deps?
+    setuptools
+  ];
+
+  makeWrapperArgs = [ # deps for `srcpkg` operation for other distros; could be optional
+    "--prefix" "PATH" ":" (lib.makeBinPath [ gitMinimal rpm dpkg fakeroot ])
   ];
 
   checkInputs = with python3Packages; [ pytest ];
